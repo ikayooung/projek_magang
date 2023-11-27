5a9093829e25940973794659248c5226d171b0c6
starduststr <starduststr0@gmail.com>
2023-11-27 08:27:50 +0700

    diable searchable

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
+// use Laravel\Scout\Searchable;
 
 class Customer extends Model
 {
     use HasFactory;
-    use Searchable;
+    // use Searchable;
 
     protected $table = 'customer';
     protected $guarded = [];
